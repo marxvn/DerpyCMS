cccd26e907df40cb2c4b64fdba9bfbc45b01764c
Diftraku <diftraku@derpy.me>
2013-07-21 12:17:25 +0300

    Add helper functions to the Page class for the template

--- DerpyCMS/Page.php
+++ DerpyCMS/Page.php
@@ -9,10 +9,33 @@ use Slim\View;
 
 class Page extends \Slim\View
 {
-    public function render($template)
+    public function content()
     {
-        $this->appendData(Blob::getParts($this->getData('id')));
-        var_dump($this->data);
-        die();
+        return $this->getData('parts.content');
+    }
+
+    public function title()
+    {
+        return $this->getData('page.title');
+    }
+
+    public function description()
+    {
+        return $this->getData('page.description');
+    }
+
+    public function keywords()
+    {
+        return $this->getData('page.keywords');
+    }
+
+    public function getPart($part)
+    {
+        $part = 'parts.'.$part;
+        if (array_key_exists($part, $this->data)) {
+            return $this->getData($part);
+        } else {
+            return '';
+        }
     }
 }
