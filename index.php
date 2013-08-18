6c1fd785ced52f0bd376460338f049a905319dca
Diftraku <diftraku@derpy.me>
2013-08-18 17:37:23 +0300

    Forgot to update the index.php for the new definition namespace

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
  * DerpyCMS - A derped CMS built on Slim
  */
 
-define('DERPY_BASE', dirname(__FILE__));
-require_once DERPY_BASE.'/config.php';
-require_once DERPY_BASE.'/vendor/autoload.php';
+define('DERPYCMS_BASE', dirname(__FILE__));
+require_once DERPYCMS_BASE.'/config.php';
+require_once DERPYCMS_BASE.'/vendor/autoload.php';
 
 $app = new \DerpyCMS\DerpyCMS();
 $app->init();
